7a96fa9d41b50a4980fdd879229dd69a7ea4493b
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2014-10-08 17:02:38 -0500

    learning how to use GULP and TRAVIS-CI

---
+++ README.md
@@ -0,0 +1,1 @@
+Status : [![Build Status](https://travis-ci.org/stlartworks/stlartworks.github.io.svg?branch=master)](https://travis-ci.org/stlartworks/stlartworks.github.io)
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+gulp.task('greet', function () {
+   console.log('Hello world!');
 });
+
+// gulp.task('build', function() {
+//     return gulp
+//         .src('css/style.css')
+//         .pipe(minifyCSS())
+//         .pipe(gule.dest('build')); //Destination
+// })    
