53daadc1ec2bf98ce3a0fe09773f25445116c782
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2014-10-09 07:36:36 -0500

    trying to get travis to pass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 var gulp = require('gulp');
 
-gulp.task('greet', function () {
-   console.log('Hello world!');
+gulp.task('default', function() {
+  // console.log('Hello world!');
 });
-
-// gulp.task('build', function() {
-//     return gulp
-//         .src('css/style.css')
-//         .pipe(minifyCSS())
-//         .pipe(gule.dest('build')); //Destination
-// })    
