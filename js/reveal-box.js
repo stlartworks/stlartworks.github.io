2d380aa2542cd592af62abe450be8fb0a76c233e
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2014-11-01 16:04:54 -0500

    building reveal box/panel many changes

--- js/reveal-box.js
+++ js/reveal-box.js
@@ -4,11 +4,11 @@ jQuery(document).ready(function($){
 	var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
 
 	//open team-member bio
-	$('#cd-team').find('ul a').on('click', function(event){
+	$('#reveal-gallery').find('ul a').on('click', function(event){
 		event.preventDefault();
 		var selected_member = $(this).data('type');
-		$('.cd-member-bio.'+selected_member+'').addClass('slide-in');
-		$('.cd-member-bio-close').addClass('is-visible');
+		$('.reveal-panel.'+selected_member+'').addClass('slide-in');
+		$('.reveal-panel-close').addClass('is-visible');
 
 		// firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
 		if( is_firefox ) {
@@ -23,10 +23,10 @@ jQuery(document).ready(function($){
 	});
 
 	//close team-member bio
-	$(document).on('click', '.cd-overlay, .cd-member-bio-close', function(event){
+	$(document).on('click', '.reveal-overlay, .reveal-panel-close', function(event){
 		event.preventDefault();
-		$('.cd-member-bio').removeClass('slide-in');
-		$('.cd-member-bio-close').removeClass('is-visible');
+		$('.reveal-panel').removeClass('slide-in');
+		$('.reveal-panel-close').removeClass('is-visible');
 
 		if( is_firefox ) {
 			$('main').removeClass('slide-out').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
