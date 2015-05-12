202f3b5c702c6bec19da6385246c5dd9a8fd78e3
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-05-12 07:34:42 -0500

    apply closed, support moved up, supprt frame homepage

--- gulpfile.js
+++ gulpfile.js
@@ -112,4 +112,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['sassprod', 'browser-sync', 'watch']);
